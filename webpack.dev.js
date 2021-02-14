d0be5391776b1f7d102d316339e8708b2d60d904
gusalbukrk <gusalbukrk@gmail.com>
2021-02-14 12:06:22 -0300

    add webpack, webpack-dev-server

---
+++ webpack.dev.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+    open: true,
+    // host: '0.0.0.0',
+  },
+});
